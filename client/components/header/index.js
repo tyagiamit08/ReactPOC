dfa260f52e71a836218323a86768ba62f8189d4f
Amit Tyagi <Amit_Tyagi@external.mckinsey.com>
2017-11-23 10:28:31 +0530

    Redesigned the header

--- client/components/header/index.js
+++ client/components/header/index.js
@@ -4,39 +4,47 @@ import Home from '../../routes/home/';
 import Weather from '../../routes/weather/';
 import Dogs from '../../routes/dogs/';
 
-import  {Navbar,NavItem,MenuItem,Nav,NavDropdown } from 'react-bootstrap';
+import  {Navbar,NavItem,MenuItem,Nav,NavDropdown,Breadcrumb,Button } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default class Header extends Component {
     render() {  
         return(
             <div>
-                {/* <div style={{'background': 'green','height': '40px','color': '#fff','font-weight': 'bold','text-align': 'center','padding-top': '9px','font-size': '20px'}}> 
-                    <span>REACT POC Application </span> 
-                </div>   */}
-                {/* <Router>
-                        <div>   
-                            <Link to={'/'}>Home</Link> | <Link to={'/Weather'}>Weather</Link> | <Link to={'/Dogs'}>Dogs</Link>
+                <Router>
+                    <div>  
+                        <div style={{'background': '#222222','height': '50px','color':'#9d9d9d','text-align': 'center','padding-top': '9px','font-size': '18px'}}> 
+                            <span style={{'margin-right': '890px'}}>React POC</span> 
+                            <span style={{'margin-right': '10px'}}> 
+                            <Link to={'/'} style={{'font-size': '15px'}}>Home</Link> | <Link to={'/Weather'} style={{'font-size': '15px'}}>Weather</Link> | <Link to={'/Dogs'} style={{'font-size': '15px'}}>Dogs</Link>  
+                            </span> 
+                        </div>    
                             <Switch>
                                 <Route exact path='/' component={Home} />
                                 <Route exact path='/Weather' component={Weather} />
                                 <Route exact path='/Dogs' component={Dogs} />
                             </Switch>
-                        </div>
-                    </Router>  */} 
-                <Navbar inverse collapseOnSelect>
+                    </div>
+                </Router>   
+                 
+                 {/* <Navbar inverse collapseOnSelect>
                     <Navbar.Header>
                     <Navbar.Brand>
-                        <a href="/">React POC</a>
+                        <a to="/">React POC</a>
                     </Navbar.Brand>
                     <Navbar.Toggle />
                     </Navbar.Header>
                     <Navbar.Collapse>
                     <Nav>
-                        <NavItem eventKey={1} href="/">Home</NavItem>
-                        <NavItem eventKey={2} href="/Weather">Weather</NavItem>
+                         <LinkContainer to={'/Weather'}>
+                                 <NavItem eventKey={1} href="/" to="/">Home</NavItem>  
+                         </LinkContainer>
+                        <NavItem eventKey={2} href="/Weather" to="/Weather">Weather</NavItem> 
                         <NavDropdown eventKey={3} title="Categories" id="basic-nav-dropdown">
-                        <MenuItem eventKey={3.1}>Dogs</MenuItem>
+                        <LinkContainer to="/Weather">
+                               <MenuItem eventKey={3.1}>Dogs</MenuItem>
+                        </LinkContainer>
                         <MenuItem eventKey={3.2}>Books</MenuItem>
                         <MenuItem eventKey={3.3}>Music</MenuItem>
                         <MenuItem divider />
@@ -48,7 +56,8 @@ export default class Header extends Component {
                         <NavItem eventKey={2} href="#">Profile</NavItem>
                     </Nav>
                     </Navbar.Collapse>
-                </Navbar>
+                </Navbar>     */}
+ 
       </div>  
         )
     }
