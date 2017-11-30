c4efdadb553cc69dc04412b1c64286598b99efda
Amit Tyagi <Amit_Tyagi@external.mckinsey.com>
2017-11-30 10:23:54 +0530

    Finished Cocktails page

--- client/routes/cocktails/index.js
+++ client/routes/cocktails/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';   
-import  {FormGroup ,FormControl ,ControlLabel,PageHeader,Table ,Image,Col,Button} from 'react-bootstrap';
+import  {FormGroup ,FormControl ,ControlLabel,PageHeader,Table ,Image,Button} from 'react-bootstrap';
 import axios from 'axios';
 
 import './style.css'
@@ -9,36 +9,73 @@ export default class Cocktail extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            drinks: [] 
+            drinks: [] ,
+            cocktails:[],
+            selectedValue:"Martini"
         } ;   
+
+        this.handleChange = this.handleChange.bind(this);
+        this.getDrink = this.getDrink.bind(this); 
       } 
 
     componentWillMount() {
-        axios.get('http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-            .then((response) => { 
-                    if(response.status==200) 
-                    console.log(response.data.drinks);
-                        this.setState({ 'drinks':response.data.drinks}); 
-                    })
-            .catch(function (error) {
-                console.log(error);
-            });
+        axios.get('http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
+        .then((response) => { 
+                if(response.status==200) {               
+                    var drinks=[];  
+                    for (var i = 0; i < response.data.drinks.length-1; i++) {
+                       drinks.push(
+                            {   name:response.data.drinks[i].strDrink,
+                                url:response.data.drinks[i].strDrinkThumb,
+                                id:response.data.drinks[i].idDrink
+                            });
+                    }    
+                    
+                    this.setState({ 'cocktails':drinks})
+                }
+            })
+        .catch(function (error) {
+            console.log(error);
+        });    
     }
 
     generateDrinksList (lists) {   
            return lists.map((list, i) =>         
                 <tr>
-                    <td><Image  src={list.strDrinkThumb} style={{'width': '100px','height': '100px'}} rounded /></td>
+                    <td><Image  src={list.strDrinkThumb} className="profile" rounded /></td>
                     <td>{list.strAlcoholic}</td>
                     <td>{list.strDrink}</td>
                     <td>{list.strInstructions}</td>
                 </tr> 
-        )
+        );
     };
+
+    generateCocktails (lists) {   
+            return lists.map((cocktail, i) =>         
+                <option value={cocktail.id}>{cocktail.name}</option>
+        );
+    };
+
+    handleChange (e) {   
+        this.setState({selectedValue:e.target.value});
+      };
+
+    getDrink(){
+        axios.get('http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='+this.state.selectedValue)
+        .then((response) => { 
+                if(response.status==200) 
+                console.log(response.data.drinks);
+                    this.setState({ 'drinks':response.data.drinks}); 
+                })
+        .catch(function (error) {
+            console.log(error);
+        }); 
+    }
 
     render() {  
 
         let drinksList= this.state.drinks !==undefined? this.generateDrinksList(this.state.drinks) :null;
+        let cocktails= this.state.cocktails !==undefined? this.generateCocktails(this.state.cocktails) :null;
 
         return( 
             
@@ -46,9 +83,11 @@ export default class Cocktail extends Component {
               <PageHeader>Cocktails <small>Browse Cocktail recipies.</small></PageHeader>
                  <form>
                        <FormGroup  controlId="txtSearch">
-                            <ControlLabel>Search cocktail by name</ControlLabel>
-                                    <FormControl type="text" placeholder="Enter Name" style={{'width': '25%'}} /> 
-                                    <Button bsStyle="primary"  className="btn-submit background" >Submit</Button>
+                            <ControlLabel>Search cocktail by name</ControlLabel>   
+                                <FormControl componentClass="select" className="select" placeholder="select" defaultValue={this.state.selectedValue}  onChange={this.handleChange} >
+                                  {cocktails}
+                                </FormControl> 
+                                    <Button bsStyle="primary"  className="btn-submit background" onClick={this.getDrink} >Submit</Button>
                         </FormGroup>    
                            
                 </form> 
@@ -62,7 +101,7 @@ export default class Cocktail extends Component {
                         </tr>
                         </thead>   
                         <tbody> 
-                            {drinksList}   
+                          {drinksList}
                         </tbody>
                   </Table>  
         </div>  
